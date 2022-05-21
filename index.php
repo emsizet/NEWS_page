203af94ab6a4d761d0b4b7535994fbf24534a5f3
emsizet <17052d@gmail.com>
2022-05-21 22:28:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -92,16 +92,5 @@ a:active {
 </div>
 
 
-<script>
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-</script>
-
 </body>
 </html>
